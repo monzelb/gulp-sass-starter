3fd0fdc8d09440b822295cb127482a1c90c14274
Brett Monzel <brett@MWA272.attlocal.net>
2020-09-14 22:22:39 -0700

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,11 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 //compile scss into css
+function style(){
+    // 1. Where is my scss file?
+    return gulp.src('./scss/**/*.scss')
+    //2. pass that file to gulp compiler
+    .pipe(sass())
+    //3. Where do we save the file to?
+    .pipe(gulp.dest('./css'))
+}
